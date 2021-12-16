48b97c8b481e46314e7c8585a894c8a346baae1e
omprakash <omprakash@digio.in>
2021-12-16 11:55:11 +0530

    initial commit for versio changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import RapiDoc from '~/rapidoc';
 import RapiDocMini from '~/rapidoc-mini';
+import DigioRapiDoc from '~/digio-rapidoc';
 import OAuthReceiver from '~/oauth-receiver';
 import JsonSchemaViewer from '~/json-schema-viewer';
 
-export default { RapiDoc };
-export { RapiDocMini, OAuthReceiver, JsonSchemaViewer };
+export default {RapiDoc, DigioRapiDoc};
+export {RapiDocMini, OAuthReceiver, JsonSchemaViewer};
